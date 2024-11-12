156d098251cfe55c1d1216b44d12e11c0d629a2c
James Donckels <donckelj@uci.edu>
2024-11-11 17:47:50 -0800

    implement and test lab6 problem1

--- src/labs/lab6/BalanceTransactions.java
+++ src/labs/lab6/BalanceTransactions.java
@@ -1,5 +1,9 @@
 package labs.lab6;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * A utility class for validating the cash on hand amount at the end of the day with the 
  * amount recorded in a file
@@ -22,7 +26,50 @@ public class BalanceTransactions {
 	 *         (either "End of day total is properly balanced." or "End of day total
 	 *         is not balanced."
 	 */
-	public static String balance(double startBalance, double endBalance, String filename) {
-		return ""; // FIX ME
+	public static String balance(double startBalance, double endBalance, String filename) 
+	{
+		double balance = startBalance;
+		
+		try
+		{
+			Scanner in = new Scanner(new File(filename));
+			while (in.hasNext())
+			{
+				in.nextInt(); // process over the invoice number
+				double cashAmount = in.nextDouble();
+				String invoiceStatus = in.next();
+				
+//				DEBUG
+//				System.out.println(cashAmount);
+				
+				// if invoice paid, subtract from balance
+				// if invoice received, add to balance
+				if (invoiceStatus.equals("P"))
+					balance -= cashAmount;
+				else if (invoiceStatus.equals("R"))
+					balance += cashAmount;
+				
+			}
+			
+			in.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.print("File: " + filename + " not found");
+			return "";
+		}
+		
+//		DEBUG
+//		System.out.println("start: " + startBalance);
+//		System.out.println("end: " + endBalance);
+//		System.out.println("actual end: " + balance);
+		
+		
+		// after all invoices have been processed, check if ending balance is as expected
+		if (Math.abs(balance - endBalance) <= 1E-3)
+		{
+			return "End of day total is properly balanced.";
+		}
+		return "End of day total is not balanced.";
 	}
 }
