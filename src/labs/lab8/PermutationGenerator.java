de390987c7642fc7a7dfe6197adaebfd37c7f720
James Donckels <donckelj@uci.edu>
2024-11-25 11:50:44 -0800

    implement and test lab8 problem3

--- src/labs/lab8/PermutationGenerator.java
+++ src/labs/lab8/PermutationGenerator.java
@@ -21,12 +21,55 @@ public class PermutationGenerator<E> {
 
 
 	/**
-	 * Gets all permutations of the string.
+	 * Gets all permutations of the list.
 	 */
 	public List<List<E>> getPermutations() {
 		List<List<E>> result = new ArrayList<>();
 		
+		// empty list has a single permutation: itself
+		if (list.size() == 0)
+		{
+			result.add(list);
+			return result;
+		}
+		
+		// loop through all list elements
+		for (int i = 0; i < list.size(); ++i)
+		{
+			List<E> shorter = new ArrayList<>();
+			// form a shorter list by removing the ith element
+			for (int j = 0; j < list.size(); ++j)
+			{
+				if (j != i) 
+				{
+					shorter.add(list.get(j));
+				}
+			}
+			
+			// DEBUG
+			// System.out.println("original: " + list);
+			// System.out.println("shorter: " + shorter);
+			
+			// generate all permutations of the shorter list
+			PermutationGenerator<E> shorterPermutationGenerator = new PermutationGenerator(shorter);
+			List<List<E>> shorterPermutations = shorterPermutationGenerator.getPermutations();
+			
+			// add the removed element to the front of each permutation of the shorter list:
+			for (List<E> elmnt : shorterPermutations)
+			{
+				ArrayList<E> temp = new ArrayList<>();
+				for (int p = 0; p < elmnt.size(); ++p)
+				{
+					temp.add(elmnt.get(p));
+				}
+				temp.add(0, list.get(i));
+				result.add(temp);
+			}
+		}
+		
+		// return all permutations
 		return result;
+		
 
 //		// The empty string has a single permutation: itself
 //		if (word.length() == 0) {
